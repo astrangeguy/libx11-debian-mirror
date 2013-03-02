851a918be91788d9cd6af1ac492afd90354d1945
Alan Coopersmith <alan.coopersmith@oracle.com>
2013-03-01 20:54:24 -0800

    Add _XEatDataWords to discard a given number of 32-bit words of reply data
    Matches the units of the length field in X protocol replies, and provides
    a single implementation of overflow checking to avoid having to replicate
    those checks in every caller.
    
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
    Reviewed-by: Matthieu Herrb <matthieu.herrb@laas.fr>
    Signed-off-by: Julien Cristau <jcristau@debian.org>

--- include/X11/Xlibint.h
+++ include/X11/Xlibint.h
@@ -883,6 +883,10 @@ extern void _XEatData(
     Display*		/* dpy */,
     unsigned long	/* n */
 );
+extern void _XEatDataWords(
+    Display*		/* dpy */,
+    unsigned long	/* n */
+);
 extern char *_XAllocScratch(
     Display*		/* dpy */,
     unsigned long	/* nbytes */
